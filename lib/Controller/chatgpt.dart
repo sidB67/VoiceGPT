8a055436de4a4cb8e31c553d5af9fcfff0399876
sidB67 <sidbahl67@gmail.com>
2022-12-28 01:21:21 +0530

    Adds message tile

--- lib/Controller/chatgpt.dart
+++ lib/Controller/chatgpt.dart
@@ -1,28 +1,36 @@
 import 'package:flutter_chatgpt_api/flutter_chatgpt_api.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:voiceai/tokens.dart';
 
 import '../Model/chatmessage.dart';
 
 class ChatGPTController extends GetxController {
-  var _api = ChatGPTApi(
-      sessionToken: 'sessionToken', clearanceToken: 'clearanceToken');
+  final _api =
+      ChatGPTApi(sessionToken: sessionToken, clearanceToken: clearanceToken);
   var messages = [].obs;
   String? _parentMessageId;
   String? _conversationId;
   Future<void> sendMessage(String message) async {
-    var newMessage = await _api.sendMessage(
-      message,
-      conversationId: _conversationId,
-      parentMessageId: _parentMessageId,
-    );
-    _conversationId = newMessage.conversationId;
-    _parentMessageId = newMessage.messageId;
-    messages.add(
-        CustomChatMessage(chatMessage: newMessage.message, senderId: 'bot'));
+    try {
+      var newMessage = await _api.sendMessage(
+        message,
+        conversationId: _conversationId,
+        parentMessageId: _parentMessageId,
+      );
+      _conversationId = newMessage.conversationId;
+      _parentMessageId = newMessage.messageId;
+      messages.add(
+          CustomChatMessage(chatMessage: newMessage.message, senderId: 'bot'));
+    } catch (e) {
+      messages.add(CustomChatMessage(
+          chatMessage: 'Something went wrong, try again', senderId: 'bot'));
+    }
   }
 
   void addUserMessage(String message) {
+    print(message);
     messages.add(CustomChatMessage(chatMessage: message, senderId: 'user'));
+    print(messages);
   }
 }
--- lib/View/homeview.dart
+++ lib/View/homeview.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:get/instance_manager.dart';
 import 'package:voiceai/Controller/chatgpt.dart';
+import 'package:voiceai/Model/chatmessage.dart';
 import 'package:voiceai/colors.dart';
 
 import '../customtextstyle.dart';
@@ -9,72 +10,138 @@ import '../customtextstyle.dart';
 class HomeView extends StatelessWidget {
   HomeView({super.key});
   final ChatGPTController _gptController = Get.put(ChatGPTController());
+  String? message;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: MyColors.surface0,
-      body: Column(
-        children: [
-          Obx(() {
-            return Expanded(
-              child: ListView.builder(
-                itemCount: _gptController.messages.length,
-                itemBuilder: (ctx, idx) {
-                  return Container();
-                },
-              ),
-            );
-          }),
-          Container(
-            margin: EdgeInsets.only(left: 16, right: 16, top: 8),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  width: 36,
-                  height: 36,
-                  margin: EdgeInsets.only(right: 8),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.white.withOpacity(.04),
-                  ),
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.arrow_back_ios_new,
-                      color: Colors.white.withOpacity(.92),
-                      size: 18,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Obx(() {
+              return Expanded(
+                child: ListView.builder(
+                  itemCount: _gptController.messages.length,
+                  itemBuilder: (ctx, idx) {
+                    return MessageTile(
+                        chatMessage: _gptController.messages[idx]);
+                  },
+                ),
+              );
+            }),
+            Container(
+              margin: EdgeInsets.only(left: 16, right: 16, top: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Flexible(
+                    child: Container(
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: MyColors.surface1,
+                        border: Border.all(width: 1, color: MyColors.white12),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: TextField(
+                        cursorColor: MyColors.purple,
+                        style: CustomTextStyle.L1.copyWith(
+                          color: Colors.white.withOpacity(.92),
+                        ),
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.only(
+                            left: 16,
+                            right: 16,
+                          ),
+                          border: InputBorder.none,
+                          hintText: 'Type your message',
+                          hintStyle: CustomTextStyle.L3
+                              .copyWith(color: MyColors.white32),
+                        ),
+                        onChanged: (value) {
+                          message = value;
+                        },
+                      ),
                     ),
                   ),
-                ),
-                Flexible(
-                  child: Container(
-                    alignment: Alignment.center,
+                  Container(
+                    width: 36,
                     height: 36,
                     decoration: BoxDecoration(
-                      color: MyColors.surface1,
-                      border: Border.all(width: 1, color: MyColors.white12),
-                      borderRadius: BorderRadius.circular(8),
+                      shape: BoxShape.circle,
                     ),
-                    child: TextField(
-                      cursorColor: MyColors.purple,
-                      style: CustomTextStyle.L1.copyWith(
+                    child: IconButton(
+                      onPressed: () {
+                        if (message != null) {
+                          _gptController.addUserMessage(message!);
+                          _gptController.sendMessage(message!);
+                        }
+                      },
+                      icon: Icon(
+                        Icons.send,
                         color: Colors.white.withOpacity(.92),
+                        size: 18,
                       ),
-                      decoration: InputDecoration(
-                        contentPadding:
-                            EdgeInsets.only(left: 16, right: 16, bottom: 16),
-                        border: InputBorder.none,
-                        hintText: 'Search',
-                        hintStyle: CustomTextStyle.L3
-                            .copyWith(color: MyColors.white32),
-                      ),
-                      onChanged: (value) {},
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MessageTile extends StatelessWidget {
+  const MessageTile({
+    Key? key,
+    required this.chatMessage,
+  }) : super(key: key);
+
+  final CustomChatMessage chatMessage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: chatMessage.senderId == 'bot' ? MyColors.white12 : null,
+      margin: EdgeInsets.only(top: 10, bottom: 10),
+      padding: EdgeInsets.only(
+        left: 16,
+      ),
+      child: Row(
+        children: [
+          chatMessage.senderId == 'bot'
+              ? Container(
+                  margin: const EdgeInsets.only(right: 16.0),
+                  child: CircleAvatar(
+                    child: Image.asset(
+                      'assets/bot.jpeg',
+                      scale: 1.5,
                     ),
                   ),
                 )
-              ],
+              : Container(
+                  margin: const EdgeInsets.only(right: 16.0),
+                  child: CircleAvatar(
+                    backgroundColor: MyColors.white4,
+                    child: Icon(
+                      Icons.person,
+                    ),
+                  ),
+                ),
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              child: Wrap(
+                children: [
+                  Text(
+                    chatMessage.chatMessage,
+                    style: CustomTextStyle.L2.copyWith(color: MyColors.white92),
+                  )
+                ],
+              ),
             ),
           ),
         ],
