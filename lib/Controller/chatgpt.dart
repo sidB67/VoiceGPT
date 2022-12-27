59e7b00f26fe1ea7f7e05985d84d0aa0c1781125
sidB67 <sidbahl67@gmail.com>
2022-12-28 01:56:53 +0530

    Adds text to speech

--- lib/Controller/chatgpt.dart
+++ lib/Controller/chatgpt.dart
@@ -2,10 +2,11 @@ import 'package:flutter_chatgpt_api/flutter_chatgpt_api.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:voiceai/tokens.dart';
-
+import 'package:text_to_speech/text_to_speech.dart';
 import '../Model/chatmessage.dart';
 
 class ChatGPTController extends GetxController {
+  final tts = TextToSpeech();
   final _api =
       ChatGPTApi(sessionToken: sessionToken, clearanceToken: clearanceToken);
   var messages = [].obs;
@@ -22,9 +23,18 @@ class ChatGPTController extends GetxController {
       _parentMessageId = newMessage.messageId;
       messages.add(
           CustomChatMessage(chatMessage: newMessage.message, senderId: 'bot'));
-    } catch (e) {
-      messages.add(CustomChatMessage(
-          chatMessage: 'Something went wrong, try again', senderId: 'bot'));
+      await speakText(newMessage.message);
+    } on Exception catch (e) {
+      print(e.toString());
+      if (e.toString() == 'Exception: Rate limited') {
+        messages.add(
+            CustomChatMessage(chatMessage: 'Rate Limited', senderId: 'bot'));
+        await speakText('Rate Limited');
+      } else {
+        messages.add(CustomChatMessage(
+            chatMessage: 'Something went wrong, try again', senderId: 'bot'));
+        await speakText('Something went wrong, Please try again');
+      }
     }
   }
 
@@ -32,5 +42,11 @@ class ChatGPTController extends GetxController {
     print(message);
     messages.add(CustomChatMessage(chatMessage: message, senderId: 'user'));
     print(messages);
+  }
+
+  Future<void> speakText(String text) async {
+    print('speaking');
+
+    await tts.speak(text);
   }
 }
